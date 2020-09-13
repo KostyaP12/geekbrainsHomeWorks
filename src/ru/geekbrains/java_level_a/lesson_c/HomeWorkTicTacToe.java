86779c33e6cc40e68a6d024467de7e0ef98a573d
Kostya Petrov <kostyapetrov12rus@gmail.com>
2020-09-13 22:04:29 +0300

    reworked the checkWin

--- src/ru/geekbrains/java_level_a/lesson_c/HomeWorkTicTacToe.java
+++ src/ru/geekbrains/java_level_a/lesson_c/HomeWorkTicTacToe.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.java_level_a.lesson_c;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ public class HomeWorkTicTacToe {
     private static final char DOT_HUMAN = 'X';
     private static final char DOT_AI = 'O';
     private static final char DOT_EMPTY = '.';
+    private static final int NUMBER_DOTS_TO_WIN = 4;
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Random RANDOM = new Random();
     private static char[][] field;
@@ -16,8 +18,8 @@ public class HomeWorkTicTacToe {
 
     //init field
     private static void initField() {
-        fieldSizeY = 3;
-        fieldSizeX = 3;
+        fieldSizeY = 5;
+        fieldSizeX = 5;
         field = new char[fieldSizeY][fieldSizeX];
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
@@ -50,7 +52,7 @@ public class HomeWorkTicTacToe {
         int x;
         int y;
         do {
-            System.out.print("Введите координаты хода X и Y (от 1 до 3) через пробел >>> ");
+            System.out.print("Введите координаты хода X и Y (от 1 до 5) через пробел >>> ");
             x = SCANNER.nextInt() - 1;
             y = SCANNER.nextInt() - 1;
         } while (!isCellValid(x, y) || !isCellEmpty(x, y));
@@ -78,19 +80,50 @@ public class HomeWorkTicTacToe {
 
     // checkWin
     private static boolean checkWin(char c) {
-        // hor
-        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
-        if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
-        if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
+        for (int offsetX = 0; offsetX < fieldSizeX - NUMBER_DOTS_TO_WIN + 1; offsetX++) {
+            for (int offsetY = 0; offsetY < fieldSizeX - NUMBER_DOTS_TO_WIN + 1; offsetY++) {
+                if (checkDiagonal(c, offsetX, offsetY) || checkLanes(c, offsetX, offsetY)) return true;
+            }
+        }
+        return false;
+    }
 
-        // ver
-        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) return true;
-        if (field[0][1] == c && field[1][1] == c && field[2][1] == c) return true;
-        if (field[0][2] == c && field[1][2] == c && field[2][2] == c) return true;
+    private static boolean checkDiagonal(char c, int x, int y) {
+        int chekLeftRightChars = 0;
+        int chekRightLeftChars = 0;
+        for (int i = 0; i < NUMBER_DOTS_TO_WIN; i++) {
+            if (field[i + y][i + x] == c) {
+                chekLeftRightChars++;
+            }
+            if (chekLeftRightChars == NUMBER_DOTS_TO_WIN)
+                return true;
 
-        // dia
-        if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
-        if (field[0][2] == c && field[1][1] == c && field[2][0] == c) return true;
+            if (field[i + y][NUMBER_DOTS_TO_WIN - i - 1 + x] == c) {
+                chekRightLeftChars++;
+            }
+            if (chekRightLeftChars == NUMBER_DOTS_TO_WIN)
+                return true;
+        }
+        return false;
+    }
+
+    private static boolean checkLanes(char c, int offsetX, int offsetY) {
+        for (int x = offsetX; x < NUMBER_DOTS_TO_WIN + offsetX; x++) {
+            int chekVerticalChars = 0;
+            int chekHorizontalChars = 0;
+            for (int y = offsetY; y < NUMBER_DOTS_TO_WIN + offsetY; y++) {
+                if (field[y][x] == c) {
+                    chekVerticalChars++;
+                }
+                if (chekVerticalChars == NUMBER_DOTS_TO_WIN)
+                    return true;
+                if (field[x][y] == c) {
+                    chekHorizontalChars++;
+                }
+                if (chekHorizontalChars == NUMBER_DOTS_TO_WIN)
+                    return true;
+            }
+        }
         return false;
     }
 
@@ -134,3 +167,4 @@ public class HomeWorkTicTacToe {
         return false;
     }
 }
+
