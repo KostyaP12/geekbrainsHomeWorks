587f8a286ebf01c3e10aba5b2d861b9ed5238e1f
Kostya Petrov <kostyapetrov12rus@gmail.com>
2020-09-15 13:48:50 +0300

    first task

---
+++ src/ru/geekbrains/java_level_a/lesson_d/HomeWork.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_level_a.lesson_d;
+
+public class HomeWork {
+    public static void main (String [] arg){
+
+    }
+}
---
+++ src/ru/geekbrains/java_level_a/lesson_d/staff.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_level_a.lesson_d;
+
+public class staff {
+    private String fullName;
+    private int salary;
+    private int birthDay;
+}
