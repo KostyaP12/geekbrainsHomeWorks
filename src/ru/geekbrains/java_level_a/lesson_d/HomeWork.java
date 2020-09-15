ffc0979762c9e65af828f78abb3a1a56f11e1417
Kostya Petrov <kostyapetrov12rus@gmail.com>
2020-09-15 22:18:20 +0300

    8 task, and HomeWork finished

--- src/ru/geekbrains/java_level_a/lesson_d/HomeWork.java
+++ src/ru/geekbrains/java_level_a/lesson_d/HomeWork.java
@@ -1,7 +1,59 @@
 package ru.geekbrains.java_level_a.lesson_d;
 
 public class HomeWork {
-    public static void main (String [] arg){
-
+    public static void main (String[] args){
+        Staff firstStaff = new Staff("Peter Jackson", 5000, 58);
+        Staff secondStaff = new Staff("Martin Scorsese", 8000, 77);
+        Staff thirdStaff = new Staff("Guy Ritchie", 4000, 52);
+        Staff fourthStaff = new Staff("Emma Watson", 2000, 30);
+        Staff fifthStaff = new Staff("Daisy Ridley", 1500, 28);
+        Staff[] staffs = {firstStaff, secondStaff, thirdStaff, fourthStaff, fifthStaff};                    // 5 задание
+        showAllStaffs(staffs);                                                                              // 4 задание
+        riseSalary(staffs);                                                                                 // 6 задание
+        showStaff(staffs);                                                                                  // 5 задание
+        int averageAge = averageAge(staffs);                                                                // 7 задание
+        int averageSalary = averageSalary(staffs);                                                          // 7 задание
+        System.out.printf("Средний возраст : %d \nСредняя зарплата : %d", averageAge, averageSalary);       // 7 задание
     }
+
+    private static void showAllStaffs(Staff[] staffs) {
+        for(int i = 0; i < staffs.length; i++){
+                System.out.printf("Name: %s %d Age: \n",
+                        staffs[i].getFullName(), staffs[i].getAge());
+        }
+    }                                                 // 4 задание
+
+    private static void showStaff(Staff[] staffs) {
+        for(int i = 0; i < staffs.length; i++){
+            if (staffs[i].getAge() > 40){
+                System.out.printf("Name: %s Salary: %d Age: %d Id: %d\n",
+                        staffs[i].getFullName(), staffs[i].getSalary(), staffs[i].getAge(), staffs[i].getId());
+            }
+        }
+    }                                                     // 5 задание
+    public static void riseSalary(Staff[] staffs){
+        for(int i = 0; i < staffs.length; i++){
+            if(staffs[i].getAge() > 50){
+                staffs[i].setSalary(5000);
+            }
+        }
+    }                                                      // 6 задание
+    public static int averageAge(Staff[] staffs){
+        int sum = 0;
+        int sumStaffs = 0;
+        for (int i = 0; i < staffs.length; i++){
+            sum += staffs[i].getAge();
+            sumStaffs++;
+        }
+        return sum/sumStaffs;
+    }                                                       // 7 задание
+    public static int averageSalary(Staff[] staffs){
+        int sum = 0;
+        int sumStaffs = 0;
+        for (int i = 0; i < staffs.length; i++){
+            sum += staffs[i].getSalary();
+            sumStaffs++;
+        }
+        return sum/sumStaffs;
+    }                                                    // 7 задание
 }
--- src/ru/geekbrains/java_level_a/lesson_d/Staff.java
+++ src/ru/geekbrains/java_level_a/lesson_d/Staff.java
@@ -1,25 +1,39 @@
+
 package ru.geekbrains.java_level_a.lesson_d;
 
-public class Staff {
-    private String fullName;
-    private int salary;
-    private int birthDay;
+import java.util.Calendar;
 
-    public String getFullName() {
+public class Staff {
+    private String fullName;                                                //Задание 1
+    private int salary;                                                     //Задание 1
+    private int birthYear;                                                  //Задание 1
+    private int id;
+    static int firstId = 0;
+
+    public int getId() {                                                    // Задание 3
+        return id;
+    }
+
+    public String getFullName() {                                           // Задание 3
         return fullName;
     }
 
-    public int getSalary() {
+    public int getSalary() {                                                // Задание 3
         return salary;
     }
 
-    public int getBirthDay() {
-        return birthDay;
+    public int getAge() {                                                   // Задание 3
+        return Calendar.getInstance().get(Calendar.YEAR) - this.birthYear;
     }
 
-    Staff(String fullName, int salary, int birthDay){
+    public void setSalary(int salary) {                                     // Задание 6
+        this.salary += salary;
+    }
+
+    Staff(String fullName, int salary, int age){    //Задание 2
         this.fullName = fullName;
         this.salary = salary;
-        this.birthDay = birthDay;
+        this.birthYear = Calendar.getInstance().get(Calendar.YEAR) - age;
+        this.id = id + firstId++;                                          // Задание 8
     }
 }
