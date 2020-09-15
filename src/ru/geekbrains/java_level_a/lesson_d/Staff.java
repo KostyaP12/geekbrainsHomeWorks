9168e148bf7f3bdb3000b2c326e4fd4fe8f0f5ed
Kostya Petrov <kostyapetrov12rus@gmail.com>
2020-09-15 14:05:31 +0300

    second task

---
+++ src/ru/geekbrains/java_level_a/lesson_d/Staff.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.java_level_a.lesson_d;
+
+public class Staff {
+    private String fullName;
+    private int salary;
+    private int birthDay;
+
+    Staff(String fullName,int salary, int birthDay){
+        this.fullName = fullName;
+        this.salary = salary;
+        this.birthDay = birthDay;
+    }
+}
--- src/ru/geekbrains/java_level_a/lesson_d/staff.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.java_level_a.lesson_d;
-
-public class staff {
-    private String fullName;
-    private int salary;
-    private int birthDay;
-}
