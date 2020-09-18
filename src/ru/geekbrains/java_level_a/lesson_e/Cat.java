06128d1824529d8011496e819ab3036be4a487a2
Kostya Petrov <kostyapetrov12rus@gmail.com>
2020-09-18 19:11:54 +0300

    create home works

--- src/ru/geekbrains/java_level_a/lesson_e/Cat.java
+++ src/ru/geekbrains/java_level_a/lesson_e/Cat.java
@@ -1,4 +1,44 @@
 package ru.geekbrains.java_level_a.lesson_e;
 
-public class Cat {
+import java.util.Random;
+
+public class Cat extends Animal {
+    private Random random = new Random();
+    private float limitRunMagnitude;
+    private float limitJumpMagnitude;
+    private int minLimitRun = 50;
+    private int maxLimitRun = 100;
+    private int minLimitJump = 5;
+    private int maxLimitJump = 20;
+
+    public float getLimitRunMagnitude() {
+        return limitRunMagnitude;
+    }
+
+    public float getLimitJumpMagnitude() {
+        return limitJumpMagnitude;
+    }
+
+    Cat(String name, String colour, int age) {
+
+        super(name, colour, age);
+
+        this.limitRunMagnitude = (float) (minLimitRun +random.nextInt(maxLimitRun - minLimitRun));
+        this.limitJumpMagnitude = (float) (minLimitJump +random.nextInt(maxLimitJump - minLimitJump))/10;
+    }
+
+    @Override
+    boolean run(float magnitude) {
+        return magnitude < this.limitRunMagnitude;
+    }
+
+    @Override
+    boolean jumpOverObstacles(float magnitude) {
+        return magnitude < this.limitJumpMagnitude;
+    }
+
+    @Override
+    boolean swim(float magnitude) {
+        return false;
+    }
 }
--- src/ru/geekbrains/java_level_a/lesson_e/Dog.java
+++ src/ru/geekbrains/java_level_a/lesson_e/Dog.java
@@ -1,4 +1,52 @@
 package ru.geekbrains.java_level_a.lesson_e;
 
-public class Dog {
+import java.util.Random;
+
+public class Dog extends Animal {
+    private Random random = new Random();
+    private float limitRunMagnitude;
+    private float limitJumpMagnitude;
+    private float limitSwimMagnitude;
+    private int minLimitRun = 400;
+    private int maxLimitRun = 600;
+    private int minLimitJump = 100;
+    private int maxLimitJump = 500;
+    private int minLimitSwim = 2;
+    private int maxLimitSwim = 15;
+
+    public float getLimitRunMagnitude() {
+        return limitRunMagnitude;
+    }
+
+    public float getLimitJumpMagnitude() {
+        return limitJumpMagnitude;
+    }
+
+    public float getLimitSwimMagnitude() {
+        return limitSwimMagnitude;
+    }
+
+    Dog(String name, String colour, int age) {
+        super(name, colour, age);
+
+        this.limitRunMagnitude = (float) (minLimitRun + random.nextInt(maxLimitRun - minLimitRun));
+        this.limitJumpMagnitude = (float) (minLimitJump + random.nextInt(maxLimitJump - minLimitJump)) / 1000;
+        this.limitSwimMagnitude = (float) (minLimitSwim + random.nextInt(maxLimitSwim - minLimitSwim));
+    }
+
+    @Override
+    boolean run(float magnitude) {
+        return magnitude < limitRunMagnitude;
+    }
+
+    @Override
+    boolean jumpOverObstacles(float magnitude) {
+        return magnitude < limitJumpMagnitude;
+    }
+
+    @Override
+    boolean swim(float magnitude) {
+        return magnitude < limitSwimMagnitude;
+    }
 }
+
