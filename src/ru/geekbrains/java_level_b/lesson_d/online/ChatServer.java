49a87d07d040151f9a6943e41e1f709ba8e1f71c
Kostya Petrov <kostyapetrov12rus@gmail.com>
2020-10-18 21:36:11 +0300

    hw

---
+++ src/ru/geekbrains/java_level_b/lesson_d/online/ChatServer.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.java_level_b.lesson_d.online;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server stared at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
